d1d672d2ab6c9dabd6e9437145ceedd279383981
gabrielc42 <clark.gabe97@gmail.com>
2023-02-17 04:20:17 -0600

    rust visitor w/ notes, dyn Visitor<&Box<Expr>> in discussion

--- rust/behavioural/visitor/src/main.rs
+++ rust/behavioural/visitor/src/main.rs
@@ -1,3 +1,78 @@
+#![allow(dead_code, unused_variables)]
+use crate::ast::*;
+use visit::*;
+
+// data we will visit
+mod ast {
+    pub enum Stmt {
+        Expr(Expr),
+        Let(Name, Expr),
+    }
+
+    pub struct Name {
+        value: String,
+    }
+
+    pub enum Expr {
+        IntLit(i64),
+        Add(Box<Expr>, Box<Expr>),
+        Sub(Box<Expr>, Box<Expr>),
+    }
+}
+
+// abstract visitor
+mod visit {
+    use crate::ast::*;
+
+    pub trait Visitor<T> {
+        fn visit_name(&mut self, n: &Name) -> T;
+        fn visit_stmt(&mut self, s: &Stmt) -> T;
+        fn visit_expr(&mut self, e: &Expr) -> T;
+    }
+}
+
+// example concrete implementation, walks AST interpreting it as code
+struct Interpreter;
+impl Visitor<i64> for Interpreter {
+    fn visit_name(&mut self, n: &Name) -> i64 {
+        panic!()
+    }
+    fn visit_stmt(&mut self, s: &Stmt) -> i64 {
+        match *s {
+            Stmt::Expr(ref e) => self.visit_expr(e),
+            Stmt::Let(..) => unimplemented!(),
+        }
+    }
+
+    fn visit_expr(&mut self, e: &Expr) -> i64 {
+        match *e {
+            Expr::IntLit(n) => n,
+            Expr::Add(ref lhs, ref rhs) => self.visit_expr(lhs) + self.visit_expr(rhs),
+            Expr::Sub(ref lhs, ref rhs) => self.visit_expr(lhs) - self.visit_expr(rhs),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // visitor pattern is good for applying algorithims to heterogeneous data
+    // homogeneous data => iterater-like pattern
+    // allows visitor to be stateful and communicate info between nodes
+    // rather than a functional approach
+
+    println!("Hello, world! 👽");
+}
+
+// factor out traversal code and share between algorithms (and provide noop default methods)
+pub fn walk_expr(visitor: &mut dyn Visitor<&Box<Expr>>, e: &Expr) {
+    match *e {
+        Expr::IntLit(_) => {}
+        Expr::Add(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+        Expr::Sub(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+    }
 }
