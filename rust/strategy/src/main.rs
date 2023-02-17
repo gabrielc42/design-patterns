c4a131a8fd118da6273bb68c34225c876af7e7b6
gabrielc42 <clark.gabe97@gmail.com>
2023-02-17 03:13:53 -0600

    rust strategy

---
+++ rust/strategy/src/main.rs
@@ -0,0 +1,98 @@
+use std::collections::HashMap;
+
+type Data = HashMap<String, u32>;
+
+trait Formatter {
+    fn format(&self, data: &Data, buf: &mut String);
+}
+
+struct Report;
+
+impl Report {
+    // Write should be used but this is String to ignore error handling
+    fn generate<T: Formatter>(g: T, s: &mut String) {
+        // backend ops
+        let mut data = HashMap::new();
+        data.insert("one".to_string(), 1);
+        data.insert("two".to_string(), 2);
+
+        // generate report
+        g.format(&data, s);
+    }
+}
+
+struct Text;
+
+impl Formatter for Text {
+    fn format(&self, data: &Data, buf: &mut String) {
+        for (k, v) in data {
+            let entry = format!("{} {}\n", k, v);
+            buf.push_str(&entry);
+        }
+    }
+}
+
+struct Json;
+
+impl Formatter for Json {
+    fn format(&self, data: &Data, buf: &mut String) {
+        buf.push('[');
+        for (k, v) in data.into_iter() {
+            let entry = format!(r#"{{"{}":"{}"}}"#, k, v);
+            buf.push_str(&entry);
+            buf.push(',');
+        }
+        if !data.is_empty() {
+            buf.pop(); // remove extra , at end
+        }
+
+        buf.push(']');
+    }
+}
+
+struct Adder;
+
+impl Adder {
+    pub fn add<F>(x: u8, y: u8, f: F) -> u8
+    where
+        F: Fn(u8, u8) -> u8,
+    {
+        f(x, y)
+    }
+}
+
+fn main() {
+    let mut s = String::from("");
+    Report::generate(Text, &mut s);
+    assert!(s.contains("one 1"));
+    assert!(s.contains("two 2"));
+
+    s.clear(); // reuse same buffer
+    Report::generate(Json, &mut s);
+    assert!(s.contains(r#"{"one":"1"}"#));
+    assert!(s.contains(r#"{"two":"2"}"#));
+
+    // strategy pattern w/ closures
+    let arith_adder = |x, y| x + y;
+    let bool_adder = |x, y| {
+        if x == 1 || y == 1 {
+            1
+        } else {
+            0
+        }
+    };
+    let custom_adder = |x, y| 2 * x + y;
+
+    assert_eq!(9, Adder::add(4, 5, arith_adder));
+    assert_eq!(0, Adder::add(0, 0, bool_adder));
+    assert_eq!(5, Adder::add(1, 3, custom_adder));
+
+    // Option's map method
+    let val = Some("Rust");
+
+    let len_strategy = |s: &str| s.len();
+    assert_eq!(4, val.map(len_strategy).unwrap());
+
+    let first_byte_strategy = |s: &str| s.bytes().next().unwrap();
+    assert_eq!(82, val.map(first_byte_strategy).unwrap());
+}
