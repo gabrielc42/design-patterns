3358789316024073b65a1e51a6ece256a8ddc662
gabrielc42 <clark.gabe97@gmail.com>
2023-03-05 21:21:38 -0600

    rust anti-patterns: .clone() and cargo clippy

---
+++ rust/anti_patterns/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Hello, world of anti patterns!");
+
+    // clone to satisfy the borrow checker
+    // define any variable
+    let mut x = 5;
+
+    // borrow `x` -- but clone it first
+    let y = &mut (x.clone());
+
+    // w/out x.clone(), following line would fail on compilation
+    println!("{}", x);
+
+    // perform some action on the borrow
+    // to prevent rust form optimizing this out of existence
+    *y += 1;
+
+    // cargo clippy detects some cases in which .clone() is not necessary
+}
