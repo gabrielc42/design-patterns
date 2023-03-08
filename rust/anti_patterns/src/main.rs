968a8fdd43bbd509de2b7ce08f9a1237aa935142
gabrielc42 <clark.gabe97@gmail.com>
2023-03-08 10:06:46 -0600

    deref polymorphism

--- rust/anti_patterns/src/main.rs
+++ rust/anti_patterns/src/main.rs
@@ -20,4 +20,64 @@ fn main() {
     // ![deny(warnings)]
     // opts out of warnings
     // RUSTFLAGS="-D warnings" cargo build
+
+    // Deref polymorphism
+    // misuse Deref trait to emulate inheritance between
+    // structs, and thus reuse methods
+
+    let b = Bar { f: Foo {} };
+    b.m();
+
+    // no struct inheritance in rust
+
+    // traits implemented by Foo are not automatically implemented for
+    // Bar, so this pattern has problems with bounds checking and thus generic programming
+
+    // only supports single inheritance,
+    // no notion of interfaces, class-based privacy,
+    // or other inheritance-related features
+
+    // depending on circumstances it might be better to
+    // re-implement using traits or write out facade methods
+    // to dispatch to Foo manually
+
+    // Deref trait is designed for implementation of customer pointer types
+    // takes a pointer-to-T to a T, not convert between types
+}
+
+// deref polymorphism
+// sometimes we want to emulate common patterns from
+// OO languages such as Java:
+// class Foo {
+//     void m() { ... }
+// }
+
+// class Bar extends Foo {}
+
+// public static void main(String[] args) {
+//     Bar b = new Bar();
+//     b.m();
+// }
+
+// can use deref polymorphism to do so:
+
+use std::ops::Deref;
+
+struct Foo {}
+
+impl Foo {
+    fn m(&self) {
+        //...
+    }
+}
+
+struct Bar {
+    f: Foo,
+}
+
+impl Deref for Bar {
+    type Target = Foo;
+    fn deref(&self) -> &Foo {
+        &self.f
+    }
 }
